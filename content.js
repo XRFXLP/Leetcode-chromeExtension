b9d02b81da28f31606b24320b1ec146ad56d7e4d
Ajay Mishra <Ajaym6405@gmail.com>
2021-07-06 21:01:43 +0530

    Add files via upload

---
+++ content.js
@@ -0,0 +1,11 @@
+window.onload = function(){
+    console.log("Running");
+    var M = document.getElementsByClassName("container__YPDh");
+    M[0].style.fontSize = '18px';
+    M[0].style.fontFamily = 'monospace';
+    M[0].style.fontWeight = '400';
+
+    var Q = document.getElementsByClassName("CodeMirror-wrap");
+    Q[0].style.backgroundColor = "#2e3440"
+    Q[0].style.color = "#f8f8f8f"
+}
